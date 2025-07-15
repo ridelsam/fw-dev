adf7c5051ab3d1d3ddf690606cef0eaff13a8151
ridelsam <ridel.asam@gmail.com>
2025-07-14 21:06:13 -0700

    APIs for IRQ config, IRQ Priority and IRQ Handling

--- drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h
+++ drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h
@@ -12,6 +12,38 @@
 
 #define __vo volatile
 
+
+/**********************************START:Processor Specific Details **********************************/
+/*
+ * ARM Cortex Mx Processor NVIC ISERx register Addresses
+ */
+
+#define NVIC_ISER0          ( (__vo uint32_t*)0xE000E100 )
+#define NVIC_ISER1          ( (__vo uint32_t*)0xE000E104 )
+#define NVIC_ISER2          ( (__vo uint32_t*)0xE000E108 )
+#define NVIC_ISER3          ( (__vo uint32_t*)0xE000E10c )
+
+
+/*
+ * ARM Cortex Mx Processor NVIC ICERx register Addresses
+ */
+#define NVIC_ICER0 			((__vo uint32_t*)0XE000E180)
+#define NVIC_ICER1			((__vo uint32_t*)0XE000E184)
+#define NVIC_ICER2  		((__vo uint32_t*)0XE000E188)
+#define NVIC_ICER3			((__vo uint32_t*)0XE000E18C)
+
+
+/*
+ * ARM Cortex Mx Processor Priority Register Address Calculation
+ */
+#define NVIC_PR_BASE_ADDR 	((__vo uint32_t*)0xE000E400)
+
+/*
+ * ARM Cortex Mx Processor number of priority bits implemented in Priority Register
+ */
+#define NO_PR_BITS_IMPLEMENTED  4
+
+
 /*
  * base addresses of Flash and SRAM memories
  */
@@ -19,14 +51,14 @@
 #define FLASH_BASEADDR						0x08000000U   		/*!<explain this macro briefly here  */
 #define SRAM1_BASEADDR						0x20000000U  		//12kB
 #define SRAM2_BASEADDR						0x2001C000U 		/*!<explain this macro briefly here  */
-#define ROM_BASEADDR						0x1FFF0000U
-#define SRAM 								SRAM1_BASEADDR
+#define ROM_BASEADDR						  0x1FFF0000U
+#define SRAM 								      SRAM1_BASEADDR
 
 /*
  * AHBx and APBx Bus Peripheral base addresses
  */
 
-#define PERIPH_BASEADDR 						0x40000000U
+#define PERIPH_BASEADDR 					  	0x40000000U
 #define APB1PERIPH_BASEADDR						PERIPH_BASEADDR
 #define APB2PERIPH_BASEADDR						0x40010000U
 #define AHB1PERIPH_BASEADDR						0x40020000U
@@ -40,13 +72,13 @@
 
 #define GPIOA_BASEADDR                   (AHB1PERIPH_BASEADDR + 0x0000)
 #define GPIOB_BASEADDR                   (AHB1PERIPH_BASEADDR + 0x0400)
-#define GPIOC_BASEADDR 					 (AHB1PERIPH_BASEADDR + 0x0800)
-#define GPIOD_BASEADDR 					 (AHB1PERIPH_BASEADDR + 0x0C00)
-#define GPIOE_BASEADDR 					 (AHB1PERIPH_BASEADDR + 0x1000)
-#define GPIOF_BASEADDR 					 (AHB1PERIPH_BASEADDR + 0x1400)
-#define GPIOG_BASEADDR 					 (AHB1PERIPH_BASEADDR + 0x1800)
-#define GPIOH_BASEADDR 					 (AHB1PERIPH_BASEADDR + 0x1C00)
-#define GPIOI_BASEADDR 					 (AHB1PERIPH_BASEADDR + 0x2000)
+#define GPIOC_BASEADDR 					         (AHB1PERIPH_BASEADDR + 0x0800)
+#define GPIOD_BASEADDR 					         (AHB1PERIPH_BASEADDR + 0x0C00)
+#define GPIOE_BASEADDR 					         (AHB1PERIPH_BASEADDR + 0x1000)
+#define GPIOF_BASEADDR 					         (AHB1PERIPH_BASEADDR + 0x1400)
+#define GPIOG_BASEADDR 					         (AHB1PERIPH_BASEADDR + 0x1800)
+#define GPIOH_BASEADDR 					         (AHB1PERIPH_BASEADDR + 0x1C00)
+#define GPIOI_BASEADDR 					         (AHB1PERIPH_BASEADDR + 0x2000)
 #define RCC_BASEADDR                     (AHB1PERIPH_BASEADDR + 0x3800)
 
 /*
@@ -144,12 +176,12 @@ typedef struct
  */
 typedef struct
 {
-	__vo uint32_t IMR;    /*!< Give a short description,          	  	    Address offset: 0x00 */
-	__vo uint32_t EMR;    /*!< TODO,                						Address offset: 0x04 */
-	__vo uint32_t RTSR;   /*!< TODO,  									     Address offset: 0x08 */
-	__vo uint32_t FTSR;   /*!< TODO, 										Address offset: 0x0C */
-	__vo uint32_t SWIER;  /*!< TODO,  									   Address offset: 0x10 */
-	__vo uint32_t PR;     /*!< TODO,                   					   Address offset: 0x14 */
+	__vo uint32_t IMR;    /*!< Give a short description,          	Address offset: 0x00 */
+	__vo uint32_t EMR;    /*!< TODO,                						    Address offset: 0x04 */
+	__vo uint32_t RTSR;   /*!< TODO,  									            Address offset: 0x08 */
+	__vo uint32_t FTSR;   /*!< TODO, 								            		Address offset: 0x0C */
+	__vo uint32_t SWIER;  /*!< TODO,  									            Address offset: 0x10 */
+	__vo uint32_t PR;     /*!< TODO,                   						  Address offset: 0x14 */
 
 }EXTI_RegDef_t;
 
@@ -183,8 +215,8 @@ typedef struct
 #define GPIOH  				((GPIO_RegDef_t*)GPIOH_BASEADDR)
 #define GPIOI  				((GPIO_RegDef_t*)GPIOI_BASEADDR)
 
-#define RCC 				((RCC_RegDef_t*)RCC_BASEADDR)
-#define EXTI				((EXTI_RegDef_t*)EXTI_BASEADDR)
+#define RCC 			  	((RCC_RegDef_t*)RCC_BASEADDR)
+#define EXTI			  	((EXTI_RegDef_t*)EXTI_BASEADDR)
 #define SYSCFG				((SYSCFG_RegDef_t*)SYSCFG_BASEADDR)
 
 
@@ -192,7 +224,7 @@ typedef struct
  * Clock Enable Macros for GPIOx peripherals
  */
 
-#define GPIOA_PCLK_EN()    	(RCC->AHB1ENR |= (1 << 0))
+#define GPIOA_PCLK_EN()   (RCC->AHB1ENR |= (1 << 0))
 #define GPIOB_PCLK_EN()		(RCC->AHB1ENR |= (1 << 1))
 #define GPIOC_PCLK_EN()		(RCC->AHB1ENR |= (1 << 2))
 #define GPIOD_PCLK_EN()		(RCC->AHB1ENR |= (1 << 3))
@@ -287,28 +319,60 @@ typedef struct
 /*
  * This macro returns a code( between 0 to 7) for a given GPIO base address(x)
  */
-#define GPIO_BASEADDR_TO_CODE(x)      ( (x == GPIOA)?0:\
-										(x == GPIOB)?1:\
-										(x == GPIOC)?2:\
-										(x == GPIOD)?3:\
-								        (x == GPIOE)?4:\
-								        (x == GPIOF)?5:\
-								        (x == GPIOG)?6:\
-								        (x == GPIOH)?7: \
-								        (x == GPIOI)?8:0)
+#define GPIO_BASEADDR_TO_CODE(x)      ((x == GPIOA)?0:\
+										                   (x == GPIOB)?1:\
+										                   (x == GPIOC)?2:\
+										                   (x == GPIOD)?3:\
+								                       (x == GPIOE)?4:\
+								                       (x == GPIOF)?5:\
+								                       (x == GPIOG)?6:\
+								                       (x == GPIOH)?7: \
+								                       (x == GPIOI)?8:0)
 
+/*
+ * IRQ(Interrupt Request) Numbers of STM32F407x MCU
+ * NOTE: update these macros with valid values according to your MCU
+ * TODO: You may complete this list for other peripherals
+ */
+
+#define IRQ_NO_EXTI0 	    	6
+#define IRQ_NO_EXTI1 		    7
+#define IRQ_NO_EXTI2 		    8
+#define IRQ_NO_EXTI3 		    9
+#define IRQ_NO_EXTI4 		    10
+#define IRQ_NO_EXTI9_5 		  23
+#define IRQ_NO_EXTI15_10 	  40
+#define IRQ_NO_SPI1			    35
+#define IRQ_NO_SPI2         36
+#define IRQ_NO_SPI3         51
+#define IRQ_NO_SPI4
+#define IRQ_NO_I2C1_EV      31
+#define IRQ_NO_I2C1_ER      32
+#define IRQ_NO_USART1	      37
+#define IRQ_NO_USART2	      38
+#define IRQ_NO_USART3	      39
+#define IRQ_NO_UART4	      52
+#define IRQ_NO_UART5	      53
+#define IRQ_NO_USART6	      71
+
+
+/*
+ * macros for all the possible priority levels
+ */
+#define NVIC_IRQ_PRI0    0
+#define NVIC_IRQ_PRI15    15
 
 
 
 
 //some generic macros
 
-#define ENABLE 				1
-#define DISABLE 			0
-#define SET 				ENABLE
-#define RESET 				DISABLE
-#define GPIO_PIN_SET        SET
-#define GPIO_PIN_RESET      RESET
+#define ENABLE 			    	1
+#define DISABLE 		    	0
+#define SET 			      	ENABLE
+#define RESET 				    DISABLE
+#define GPIO_PIN_SET      SET
+#define GPIO_PIN_RESET    RESET
 
 
 #include "stm32f407xx_gpio_driver.h"
--- drivers/stm32f4xx_drivers/drivers/Src/stm32f407xx_gpio_driver.c
+++ drivers/stm32f4xx_drivers/drivers/Src/stm32f407xx_gpio_driver.c
@@ -366,10 +366,109 @@ void GPIO_ToggleOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber)
 }
 
 
+/*********************************************************************
+ * @fn      		  - GPIO_IRQConfig
+ *
+ * @brief             -
+ *
+ * @param[in]         -
+ * @param[in]         -
+ * @param[in]         -
+ *
+ * @return            -
+ *
+ * @Note              -
 
-/*
- * IRQ Configuration and ISR handling
  */
-void GPIO_IRQInterruptConfig(uint8_t IRQNumber, uint8_t EnorDi);
-void GPIO_IRQPriorityConfig(uint8_t IRQNumber, uint32_t IRQPriority);
-void GPIO_IRQHandling(uint8_t PinNumber);
+void GPIO_IRQInterruptConfig(uint8_t IRQNumber, uint8_t EnorDi)
+{
+
+	if(EnorDi == ENABLE)
+	{
+		if(IRQNumber <= 31)
+		{
+			//program ISER0 register
+			*NVIC_ISER0 |= ( 1 << IRQNumber );
+
+		}else if(IRQNumber > 31 && IRQNumber < 64 ) //32 to 63
+		{
+			//program ISER1 register
+			*NVIC_ISER1 |= ( 1 << (IRQNumber % 32) );
+		}
+		else if(IRQNumber >= 64 && IRQNumber < 96 )
+		{
+			//program ISER2 register //64 to 95
+			*NVIC_ISER2 |= ( 1 << (IRQNumber % 64) );
+		}
+	}else
+	{
+		if(IRQNumber <= 31)
+		{
+			//program ICER0 register
+			*NVIC_ICER0 |= ( 1 << IRQNumber );
+		}else if(IRQNumber > 31 && IRQNumber < 64 )
+		{
+			//program ICER1 register
+			*NVIC_ICER1 |= ( 1 << (IRQNumber % 32) );
+		}
+		else if(IRQNumber >= 64 && IRQNumber < 96 )
+		{
+			//program ICER2 register
+			*NVIC_ICER2 |= ( 1 << (IRQNumber % 64) );
+		}
+	}
+
+}
+
+/*********************************************************************
+ * @fn      		  - GPIO_IRQPriorityConfig
+ *
+ * @brief             -
+ *
+ * @param[in]         -
+ * @param[in]         -
+ * @param[in]         -
+ *
+ * @return            -
+ *
+ * @Note              -
+
+ */
+void GPIO_IRQPriorityConfig(uint8_t IRQNumber,uint32_t IRQPriority)
+{
+	//1. first lets find out the ipr register
+	uint8_t iprx = IRQNumber / 4;
+	uint8_t iprx_section  = IRQNumber %4 ;
+
+	uint8_t shift_amount = ( 8 * iprx_section) + ( 8 - NO_PR_BITS_IMPLEMENTED) ;
+
+	*(  NVIC_PR_BASE_ADDR + iprx ) |=  ( IRQPriority << shift_amount );
+
+}
+
+
+
+/*********************************************************************
+ * @fn      		  - GPIO_IRQHandling
+ *
+ * @brief             -
+ *
+ * @param[in]         -
+ * @param[in]         -
+ * @param[in]         -
+ *
+ * @return            -
+ *
+ * @Note              -
+
+ */
+void GPIO_IRQHandling(uint8_t PinNumber)
+{
+	//clear the exti pr register corresponding to the pin number
+	if(EXTI->PR & ( 1 << PinNumber))
+	{
+		//clear
+		EXTI->PR |= ( 1 << PinNumber);
+	}
+
+}
