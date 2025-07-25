8eeeb3bbab95ef1bd3388733d27e191bf558f4f0
ridelsam <ridel.asam@gmail.com>
2025-07-24 20:40:15 -0700

    I2C slave mode APIs addition in .h and .c driver files

--- drivers/stm32f4xx_drivers/drivers/Src/stm32f407xx_i2c_driver.c
+++ drivers/stm32f4xx_drivers/drivers/Src/stm32f407xx_i2c_driver.c
@@ -985,3 +985,12 @@ void I2C_ER_IRQHandling(I2C_Handle_t *pI2CHandle)
 
 }
 
+void I2C_SlaveSendData(I2C_RegDef_t *pI2C,uint8_t data)
+{
+	pI2C->DR = data;
+}
+
+uint8_t I2C_SlaveReceiveData(I2C_RegDef_t *pI2C)
+{
+    return (uint8_t) pI2C->DR;
+}
