cd15e668351799e495d8e315aded62800cda4547
ridelsam <ridel.asam@gmail.com>
2025-10-25 18:17:12 -0700

    added USART handle; bit definition for USART registers; RCC header and source files; UART setbaudrate API

--- drivers/stm32f4xx_drivers/Debug/Src/013Ii2c_slave_tx_string.d
+++ drivers/stm32f4xx_drivers/Debug/Src/013Ii2c_slave_tx_string.d
@@ -3,9 +3,13 @@ Src/013Ii2c_slave_tx_string.o: ../Src/013Ii2c_slave_tx_string.c \
  C:/Users/ridel/Documents/fw-dev/drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx_gpio_driver.h \
  C:/Users/ridel/Documents/fw-dev/drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h \
  C:/Users/ridel/Documents/fw-dev/drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx_spi_driver.h \
- C:/Users/ridel/Documents/fw-dev/drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx_i2c_driver.h
+ C:/Users/ridel/Documents/fw-dev/drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx_i2c_driver.h \
+ C:/Users/ridel/Documents/fw-dev/drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx_usart_driver.h \
+ C:/Users/ridel/Documents/fw-dev/drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx_rcc_driver.h
 C:/Users/ridel/Documents/fw-dev/drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h:
 C:/Users/ridel/Documents/fw-dev/drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx_gpio_driver.h:
 C:/Users/ridel/Documents/fw-dev/drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h:
 C:/Users/ridel/Documents/fw-dev/drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx_spi_driver.h:
 C:/Users/ridel/Documents/fw-dev/drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx_i2c_driver.h:
+C:/Users/ridel/Documents/fw-dev/drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx_usart_driver.h:
+C:/Users/ridel/Documents/fw-dev/drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx_rcc_driver.h:
--- drivers/stm32f4xx_drivers/Debug/drivers/Src/stm32f407xx_i2c_driver.d
+++ drivers/stm32f4xx_drivers/Debug/drivers/Src/stm32f407xx_i2c_driver.d
@@ -4,9 +4,13 @@ drivers/Src/stm32f407xx_i2c_driver.o: \
  C:/Users/ridel/Documents/fw-dev/drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h \
  C:/Users/ridel/Documents/fw-dev/drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx_gpio_driver.h \
  C:/Users/ridel/Documents/fw-dev/drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx_spi_driver.h \
- C:/Users/ridel/Documents/fw-dev/drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx_i2c_driver.h
+ C:/Users/ridel/Documents/fw-dev/drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx_i2c_driver.h \
+ C:/Users/ridel/Documents/fw-dev/drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx_usart_driver.h \
+ C:/Users/ridel/Documents/fw-dev/drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx_rcc_driver.h
 C:/Users/ridel/Documents/fw-dev/drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx_i2c_driver.h:
 C:/Users/ridel/Documents/fw-dev/drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h:
 C:/Users/ridel/Documents/fw-dev/drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx_gpio_driver.h:
 C:/Users/ridel/Documents/fw-dev/drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx_spi_driver.h:
 C:/Users/ridel/Documents/fw-dev/drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx_i2c_driver.h:
+C:/Users/ridel/Documents/fw-dev/drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx_usart_driver.h:
+C:/Users/ridel/Documents/fw-dev/drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx_rcc_driver.h:
