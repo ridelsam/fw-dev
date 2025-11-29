151119e68e288dd68187152ff6d7b03d94c93de9
ridelsam <ridel.asam@gmail.com>
2025-11-28 17:27:45 -0800

    Clean up repo: remove build artifacts and ignored files

---
+++ LwIP/00_base_project/Core/Inc/uart.h
@@ -0,0 +1,12 @@
+#ifndef __UART_H_
+#define __UART_H_
+
+
+#include "main.h"
+void uart3_tx_init(void);
+
+
+
+
+#endif /* __UART_H_ */  
+
---
+++ LwIP/03_uart_config/Core/Inc/uart.h
@@ -0,0 +1,11 @@
+ifndef __UART_H_
+#define __UART_H_
+
+
+#include "main.h"
+
+
+
+
+#endif /* __UART_H_ */  
+
